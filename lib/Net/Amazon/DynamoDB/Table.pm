eb08b041f016b2bfc63e295a078861826440ed70
Eric Johnson <eric.git@iijo.org>
2016-03-04 21:09:59 -0500

    Checking in changes prior to tagging of version 0.03.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 86a2d57..a056d8b 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,9 @@ Revision history for Perl extension Net-Amazon-DynamoDB-Table
    
     {{$NEXT}}
    
    +0.03 2016-03-05T02:09:55Z
    +    - bug fix to make timeout attribute actually work this time
    +
     0.02 2016-03-05T01:33:06Z
         - added a timeout attribute
         - now require N::A::DynamoDB::Lite v0.05 (was v0.03)

--- lib/Net/Amazon/DynamoDB/Table.pm
+++ lib/Net/Amazon/DynamoDB/Table.pm
@@ -7,7 +7,7 @@ use Moo;
 use Try::Tiny;
 use Test::Deep::NoTest;
 
-our $VERSION="0.02";
+our $VERSION="0.03";
 
 has table       => (is => 'rw', required => 1);
 has hash_key    => (is => 'rw', required => 1);
